a1be4be07471ed584416b916311e49a35009527f
devin <117699301+dmunoz005@users.noreply.github.com>
2024-05-11 17:29:11 -0400

    sell page work in progress

---
+++ models/sell.py
@@ -0,0 +1,24 @@
+from firebase_admin import firestore
+
+class Sneaker:
+    def __init__(self, title, description, shoe_size, condition, price, photos):
+        self.title = title
+        self.description = description
+        self.shoe_size = shoe_size
+        self.condition = condition
+        self.price = price
+        self.photos = photos
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "shoe_size": self.shoe_size,
+            "condition": self.condition,
+            "price": self.price,
+            "photos": self.photos
+        }
+
+def sneaker_listing(sneaker):
+    sneakers_collection = firestore.client().collection('sneakers')
+    sneakers_collection.add(sneaker.to_dict())
